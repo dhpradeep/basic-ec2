e2072f396895eef1dff60c333b1bd381217cac92
pradip <dhpradeep25@gmail.com>
2021-08-12 23:33:21 +0545

    changes execute

--- execute.sh
+++ execute.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh ubuntu@18.118.77.242 " sudo docker image prune -f 
-        cd shangrila-web-application 
+ssh ubuntu@18.118.77.242 " sudo cd shangrila-web-application 
         sudo docker-compose down
         git pull origin development
         sudo docker-compose up --build -d
